fa45df1108f02dff58e3a3754c45048d615401f5
Clint Worrell <github@clintworrell.com>
2017-07-02 16:05:45 -0700

    Add urlpattern for line items

--- simplelegalhw/simplelegal/urls.py
+++ simplelegalhw/simplelegal/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^invoices/(\d+)/$', views.line_items, name='line_items'),
     url(r'^invoices/', views.invoices, name='invoices'),
     url(r'^seed_invoice_model/', views.seed_invoice_model, name='seed_invoice_model'),
     url(r'^seed_lineitem_model/', views.seed_lineitem_model, name='seed_lineitem_model')
