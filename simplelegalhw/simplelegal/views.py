077a050a1e79d49c66d42d82d1af5b7444da9803
Clint Worrell <github@clintworrell.com>
2017-07-02 14:41:12 -0700

    Create view for seeding invoice model with data from API; modify how to look for API Auth Token

--- simplelegalhw/simplelegal/views.py
+++ simplelegalhw/simplelegal/views.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import json, os, urllib2
+
+from django.conf import settings
+from django.http import HttpResponse
 from django.shortcuts import render
 
-import json, os, urllib2
+from simplelegal.models import Invoice
 
 # Create your views here.
 def get_invoice_api_data():
-    with open('./auth_token.txt') as f:
+    auth_token_file = os.path.join(settings.PROJECT_ROOT, 'auth_token.txt')
+    with open(auth_token_file) as f:
         AUTH_TOKEN = f.read().strip()
 
     url = 'https://falcon.simplelegal.com/api/v1/invoices/'
@@ -16,6 +21,35 @@ def get_invoice_api_data():
     response = urllib2.urlopen(request)
     data = json.load(response)
 
-    print data
+    return data
 
-get_invoice_api_data()
+def seed_invoice_model(request):
+    data = get_invoice_api_data()
+    invoices = data.get('results')
+
+    for invoice in invoices:
+        curr_invoice = Invoice()
+        curr_invoice.simplelegal_id = invoice['id']
+        curr_invoice.invoice_number = invoice['invoice_number']
+        curr_invoice.invoice_date = invoice['invoice_date']
+        curr_invoice.status = invoice['status']
+        curr_invoice.received_date = invoice['received_date']
+        curr_invoice.billing_start_date = invoice['billing_start_date']
+        curr_invoice.billing_end_date = invoice['billing_end_date']
+        curr_invoice.total = invoice['total']
+        curr_invoice.net = invoice['net']
+        curr_invoice.tax = invoice['tax']
+        curr_invoice.currency = invoice['currency']
+        curr_invoice.exchange_rate = invoice['exchange_rate']
+        curr_invoice.native_currency = invoice['native_currency']
+        curr_invoice.native_total = invoice['native_total']
+        curr_invoice.native_net = invoice['native_net']
+        curr_invoice.native_tax = invoice['native_tax']
+        curr_invoice.approvers = invoice['approvers']
+        curr_invoice.vendor = invoice['vendor']
+        curr_invoice.vendor_id = invoice['vendor_id']
+        curr_invoice.api_url = invoice['api_url']
+        curr_invoice.url = invoice['url']
+        curr_invoice.save()
+
+    return HttpResponse("Invoice model seed complete...")
