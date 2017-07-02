6c55fc51333087041c57ba699cd1d4a5e5168a26
Clint Worrell <github@clintworrell.com>
2017-07-02 15:26:12 -0700

    Add urlpattern for seeding line item model

--- simplelegalhw/simplelegal/urls.py
+++ simplelegalhw/simplelegal/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^seed_invoice_model/', views.seed_invoice_model, name='seed_invoice_model')
+    url(r'^seed_invoice_model/', views.seed_invoice_model, name='seed_invoice_model'),
+    url(r'^seed_lineitem_model/', views.seed_lineitem_model, name='seed_lineitem_model')
 ]
