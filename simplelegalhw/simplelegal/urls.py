be90e72cf97931d1fa724440870adff38571ed8c
Clint Worrell <github@clintworrell.com>
2017-07-02 15:54:32 -0700

    Add urlpattern for invoices

--- simplelegalhw/simplelegal/urls.py
+++ simplelegalhw/simplelegal/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^invoices/', views.invoices, name='invoices'),
     url(r'^seed_invoice_model/', views.seed_invoice_model, name='seed_invoice_model'),
     url(r'^seed_lineitem_model/', views.seed_lineitem_model, name='seed_lineitem_model')
 ]
