4503ad1d0eae40fe5aacf73ee27fd9fc8b08c89a
Clint Worrell <github@clintworrell.com>
2017-07-01 19:16:20 -0700

    Create file to read API Authorization Token from; retrieve JSON response from Invoices endpoint

--- simplelegalhw/simplelegal/views.py
+++ simplelegalhw/simplelegal/views.py
@@ -3,4 +3,19 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+import json, os, urllib2
+
 # Create your views here.
+def get_invoice_api_data():
+    with open('./auth_token.txt') as f:
+        AUTH_TOKEN = f.read().strip()
+
+    url = 'https://falcon.simplelegal.com/api/v1/invoices/'
+    request = urllib2.Request(url)
+    request.add_header('Authorization', 'Token ' + AUTH_TOKEN)
+    response = urllib2.urlopen(request)
+    data = json.load(response)
+
+    print data
+
+get_invoice_api_data()
