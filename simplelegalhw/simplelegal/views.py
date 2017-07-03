9cb0727560194de9065d7c9f2a01381345d1b6ef
Clint Worrell <github@clintworrell.com>
2017-07-02 17:42:21 -0700

    Move database seeding from view to Model Managers

--- simplelegalhw/simplelegal/views.py
+++ simplelegalhw/simplelegal/views.py
@@ -37,32 +37,7 @@ def line_items(request, invoice_number):
 
 def seed_invoice_model(request):
     data = get_invoice_api_data()
-    invoices = data.get('results')
-
-    for invoice in invoices:
-        curr_invoice = Invoice()
-        curr_invoice.simplelegal_id = invoice['id']
-        curr_invoice.invoice_number = invoice['invoice_number']
-        curr_invoice.invoice_date = invoice['invoice_date']
-        curr_invoice.status = invoice['status']
-        curr_invoice.received_date = invoice['received_date']
-        curr_invoice.billing_start_date = invoice['billing_start_date']
-        curr_invoice.billing_end_date = invoice['billing_end_date']
-        curr_invoice.total = invoice['total']
-        curr_invoice.net = invoice['net']
-        curr_invoice.tax = invoice['tax']
-        curr_invoice.currency = invoice['currency']
-        curr_invoice.exchange_rate = invoice['exchange_rate']
-        curr_invoice.native_currency = invoice['native_currency']
-        curr_invoice.native_total = invoice['native_total']
-        curr_invoice.native_net = invoice['native_net']
-        curr_invoice.native_tax = invoice['native_tax']
-        curr_invoice.approvers = invoice['approvers']
-        curr_invoice.vendor = invoice['vendor']
-        curr_invoice.vendor_id = invoice['vendor_id']
-        curr_invoice.api_url = invoice['api_url']
-        curr_invoice.url = invoice['url']
-        curr_invoice.save()
+    Invoice.objects.seed_invoices(data)
 
     return HttpResponse("Invoice model seed complete...")
 
@@ -80,29 +55,6 @@ def seed_lineitem_model(request):
 
         invoice_number = data['invoice_number']
         lines = data['lines']
-        for line in lines:
-            current_line = LineItem()
-            current_line.line_item_number = line['line_item_number']
-            current_line.invoice_number = invoice_number
-            current_line.description = line['description']
-            current_line.law_firm_matter_id = line['law_firm_matter_id']
-            current_line.client_id = line['client_id']
-            current_line.line_item_type = line['line_item_type']
-            current_line.units = line['units']
-            current_line.rate = line['rate']
-            current_line.adjustment = line['adjustment']
-            current_line.total = line['total']
-            current_line.date = line['date']
-            current_line.task_code = line['task_code']
-            current_line.expense_code = line['expense_code']
-            current_line.activity_code = line['activity_code']
-            current_line.timekeeper_id = line['timekeeper_id']
-            current_line.timekeeper = line['timekeeper']
-            current_line.timekeeper_level = line['timekeeper_level']
-            current_line.native_tax = line['native_tax']
-            current_line.native_rate = line['native_rate']
-            current_line.native_adjustment = line['native_adjustment']
-            current_line.native_total = line['native_total']
-            current_line.save()
+        LineItem.objects.seed_line_items(invoice_number, lines)
 
     return HttpResponse("LineItem model seed complete...")
