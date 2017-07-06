fb8f7fcb4ddf8c0abe37dcdbce3b38d57fd15d89
Clint Worrell <github@clintworrell.com>
2017-07-06 01:25:25 -0700

    Add urlpattern for invoice filter AJAX request

--- simplelegalhw/simplelegal/urls.py
+++ simplelegalhw/simplelegal/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^invoices/(\d+)/$', views.line_items, name='line_items'),
+    url(r'^invoices/filter=(\w*)/$', views.filter_invoices, name='filter_invoices'),
     url(r'^invoices/', views.invoices, name='invoices'),
     url(r'^seed_invoice_model/', views.seed_invoice_model, name='seed_invoice_model'),
     url(r'^seed_lineitem_model/', views.seed_lineitem_model, name='seed_lineitem_model')
